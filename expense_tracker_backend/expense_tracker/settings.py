56c34b2f4d4b3a83f32afb5048bfa2dbb885c49d
DipeshJungThapa <dipsont123@gmail.com>
2026-01-19 21:45:53 +0545

    feat : integrated psql using dbeaver

--- expense_tracker_backend/expense_tracker/settings.py
+++ expense_tracker_backend/expense_tracker/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='expense_db'),
+        'USER': config('DB_USER', default='dipesh'),
+        'PASSWORD': config('DB_PASSWORD', default='dipesh'),
+        'HOST': config('DB_HOST', default='127.0.0.1'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
